ee2ead79423538a54a424c286d1f8bfe5e2a4665
Felix murimi <61313608+Felix280murimi@users.noreply.github.com>
2020-09-25 13:22:27 +0300

    Add project files.

---
+++ Even numbers and their sum/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnSum = New System.Windows.Forms.Button()
+        Me.listEvenAndTotal = New System.Windows.Forms.ListBox()
+        Me.lblResults = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(36, 415)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(75, 23)
+        Me.btnDisplay.TabIndex = 0
+        Me.btnDisplay.Text = "Display"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'btnSum
+        '
+        Me.btnSum.Location = New System.Drawing.Point(341, 415)
+        Me.btnSum.Name = "btnSum"
+        Me.btnSum.Size = New System.Drawing.Size(75, 23)
+        Me.btnSum.TabIndex = 1
+        Me.btnSum.Text = "Sum"
+        Me.btnSum.UseVisualStyleBackColor = True
+        '
+        'listEvenAndTotal
+        '
+        Me.listEvenAndTotal.FormattingEnabled = True
+        Me.listEvenAndTotal.ItemHeight = 16
+        Me.listEvenAndTotal.Location = New System.Drawing.Point(36, 43)
+        Me.listEvenAndTotal.Name = "listEvenAndTotal"
+        Me.listEvenAndTotal.Size = New System.Drawing.Size(380, 324)
+        Me.listEvenAndTotal.TabIndex = 2
+        '
+        'lblResults
+        '
+        Me.lblResults.AutoSize = True
+        Me.lblResults.Location = New System.Drawing.Point(209, 336)
+        Me.lblResults.Name = "lblResults"
+        Me.lblResults.Size = New System.Drawing.Size(0, 17)
+        Me.lblResults.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(500, 450)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.lblResults)
+        Me.Controls.Add(Me.listEvenAndTotal)
+        Me.Controls.Add(Me.btnSum)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents btnSum As Button
+    Friend WithEvents listEvenAndTotal As ListBox
+    Friend WithEvents lblResults As Label
+End Class
---
+++ Even numbers and their sum/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub btnSum_Click(sender As Object, e As EventArgs) Handles btnSum.Click
+        Dim i As Integer
+        Dim numsum As Integer
+        numsum = 0
+        For i = 2 To 20 Step 2
+            numsum = numsum + i
+        Next
+        lblResults.Text = numsum
+    End Sub
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        listEvenAndTotal.Items.Clear()
+        Dim a As Boolean
+        For i = 2 To 20 Step 2
+            If i Mod 2 = 0 Then
+                a = False
+            Else
+                a = True
+            End If
+            If a = True Then
+                listEvenAndTotal.Items.Add(i)
+            End If
+        Next
+    End Sub
+End Class
